b07fa38b7a205ddf9c0fceda00530631e19caa3d
ethan-zajaczkowski <ethan.zajaczkowski@mail.utoronto.ca>
2023-10-03 20:08:31 -0400

    Created dataAccessInterface package, and moved code from main into this package. The main function now acts dynamically, so we can retrieve any company's financial data by simply putting in their ticker

---
+++ yahoo-finance-api-example/src/dataAccessInterface/FinancialDataRetriever.java
@@ -0,0 +1,14 @@
+package src.financialDataRetriever;
+import org.json.JSONObject;
+
+public class financialDataRetriever {
+    public String ticker;
+    public String urlMain;
+
+    public financialDataRetriever(String companyTicker) {
+        this.ticker = companyTicker;
+        this.urlMain = String.format("https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/%s/financial-data", this.ticker);
+    }
+
+
+}
