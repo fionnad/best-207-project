e6c0f854e3c89fd36322ca18eb227a878baf78fb
anguskongyeung <angusk.yeung@mail.utoronto.ca>
2023-10-23 14:03:31 -0400

    Started on creating classes in for use_cases in order to make our UML diagram and sequence. Organized the src files for our actual app.

--- src/use_case/CalculateTerminalValue/CalculateTerminalValue.java
+++ src/use_case/CalculateTerminalValue/CalculateTerminalValue.java
@@ -1,4 +1,4 @@
-package use_case;
+package use_case.CalculateTerminalValue;
 
 public class CalculateTerminalValue {
 }
---
+++ src/use_case/GetDCF/getDCFInputData.java
@@ -0,0 +1,4 @@
+package use_case.GetDCF;
+
+public class getDCFInputData {
+}
