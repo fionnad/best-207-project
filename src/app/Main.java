88b55e7dae783119e3c360bb09732a70cd23d675
anguskongyeung <angusk.yeung@mail.utoronto.ca>
2023-11-09 15:13:56 -0800

    Started on creating classes in for use_cases in order to make our UML diagram and sequence. Organized the src files for our actual app.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,37 @@
 package app;
 
+import interface_adapter.FirstPage.FirstPageViewModel;
+import view.FirstPageView;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) {
+        // Build the main program window, the main panel containing the
+        // various cards, and the layout, and stitch them together.
+
+        // The main application window.
+        JFrame application = new JFrame("best-207-project");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+
+        // The various View objects. Only one view is visible at a time.
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        // The data for the views, such as username and password, are in the ViewModels.
+        // This information will be changed by a presenter object that is reporting the
+        // results from the use case. The ViewModels are observable, and will
+        // be observed by the Views.
+        FirstPageViewModel firstPageViewModel = new FirstPageViewModel();
+
+        FirstPageView firstPageView = new FirstPageView(firstPageViewModel);
+        views.add(firstPageView);
+
+        application.pack();
+        application.setVisible(true);
+    }
 }
