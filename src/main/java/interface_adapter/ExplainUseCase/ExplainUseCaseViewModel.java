9201b942c998c04d02eb9148b7bc44d7c4057190
aprilywK <aprilywk@gmail.com>
2023-11-24 22:54:51 -0500

    Added definitions for each financial term

--- src/main/java/interface_adapter/ExplainUseCase/ExplainUseCaseViewModel.java
+++ src/main/java/interface_adapter/ExplainUseCase/ExplainUseCaseViewModel.java
@@ -1,11 +1,20 @@
 package interface_adapter.ExplainUseCase;
 
 import interface_adapter.ViewModel;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 public class ExplainUseCaseViewModel extends ViewModel {
     public static final String TITLE_LABEL = "Understanding Financial Terms";
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
     public ExplainUseCaseViewModel() {
         super("Financial Terminology Definitions");
     }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+
 }
--- src/main/java/view/ExplainUseCaseView.java
+++ src/main/java/view/ExplainUseCaseView.java
@@ -5,56 +5,46 @@ import interface_adapter.FirstPage.FirstPageViewModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ExplainUseCaseView extends JPanel {
     private final ExplainUseCaseViewModel explainUseCaseViewModel;
-    private final JTextField financialTermsDefnition = new JTextField(15);
-
 
     public ExplainUseCaseView(ExplainUseCaseViewModel explainUseCaseViewModel) {
         this.explainUseCaseViewModel = explainUseCaseViewModel;
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         JLabel title = new JLabel(ExplainUseCaseViewModel.TITLE_LABEL);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
-        this.add(title);
+        add(title);
 
-        // Button for each term
-        JPanel debtButton = new JPanel();
-        JButton financialTerms = new JButton("Debt");
-        debtButton.add(financialTerms);
+        // Upper Button Panel
+        JPanel upperButtonPanel = new JPanel();
+        upperButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JButton debtButton = new JButton("Debt");
+        debtButton.addActionListener(e -> showDefinition("Debt", "Debt is something one party owes to another."));
+        JButton equityButton = new JButton("Equity");
+        equityButton.addActionListener(e -> showDefinition("Equity", "Equity represents the value that would be returned to shareholders if all assets were liquidated."));
+        upperButtonPanel.add(debtButton);
+        upperButtonPanel.add(equityButton);
 
-        JPanel equityButton = new JPanel();
-        JButton financialTerms2 = new JButton("Equity");
-        equityButton.add(financialTerms2);
+        // Lower Button Panel
+        JPanel lowerButtonPanel = new JPanel();
+        lowerButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JButton debtToEquityButton = new JButton("Debt to Equity");
+        debtToEquityButton.addActionListener(e -> showDefinition("Debt to Equity", "Debt to Equity ratio indicates the relative proportion of equity and debt used to finance a company's assets."));
+        JButton ebitdaMarginButton = new JButton("EBITDA Margin");
+        ebitdaMarginButton.addActionListener(e -> showDefinition("EBITDA Margin", "EBITDA Margin is a measure of a company's operating profit as a percentage of its revenue."));
+        lowerButtonPanel.add(debtToEquityButton);
+        lowerButtonPanel.add(ebitdaMarginButton);
 
-        JPanel deButton = new JPanel();
-        JButton financialTerms3 = new JButton("Debt to Equity");
-        deButton.add(financialTerms3);
+        // Adding Button Panels to Main Panel
+        add(upperButtonPanel);
+        add(lowerButtonPanel);
+    }
 
-        JPanel ebitdaButton = new JPanel();
-        JButton financialTerms4 = new JButton("EBITDA Margin");
-        ebitdaButton.add(financialTerms4);
-
-        // Main Panel to Hold All Rows (Nested Panels)
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS)); // Use BoxLayout for vertical arrangement
-//        mainPanel.add(titleRowPanel);
-//        mainPanel.add(searchRowPanel);
-        mainPanel.add(debtButton);
-        mainPanel.add(equityButton);
-        mainPanel.add(deButton);
-        mainPanel.add(ebitdaButton);
-//        mainPanel.add(this.informationPresentedPanel);
-
-        // Adding Main to View
-        this.setLayout(new BorderLayout());
-        this.add(mainPanel, BorderLayout.NORTH);
-
-
-
-
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-
+    private void showDefinition(String term, String definition) {
+        JOptionPane.showMessageDialog(this, definition, term + " Definition", JOptionPane.INFORMATION_MESSAGE);
     }
 }
