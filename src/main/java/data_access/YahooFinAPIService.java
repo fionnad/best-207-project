fc38d0b60f30a7332d63add69dc6e29e33e8ac2f
anguskongyeung <angusk.yeung@mail.utoronto.ca>
2023-11-28 23:49:30 -0500

    Fionn and I fixed the stupid new update on the API

--- src/main/java/data_access/YahooFinAPIService.java
+++ src/main/java/data_access/YahooFinAPIService.java
@@ -9,12 +9,12 @@ public class YahooFinAPIService {
     public static String getFinData(String ticker) {
         try {
             OkHttpClient client = new OkHttpClient();
-            String url = String.format("https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/%s/financial-data", ticker);
+            String url = String.format("https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/modules?ticker=%s&module=financial-data", ticker);
 
             Request request = new Request.Builder()
                     .url(url)
                     .get()
-                    .addHeader("X-RapidAPI-Key", "9b0be126c5msh89b252e1f24238cp1ac534jsn35fcd4caef3e")
+                    .addHeader("X-RapidAPI-Key", "7aa0af6cf0mshc69956a71e31b89p19cb70jsn7137ce116466")
                     .addHeader("X-RapidAPI-Host", "yahoo-finance15.p.rapidapi.com")
                     .build();
 
